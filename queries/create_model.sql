99ed5652f4f63598327978d51d20303e42d81ef1
Chris Suarez <chris.jsa01@gmail.com>
2020-06-19 22:31:15 -0400

    prueba de git

--- queries/create_model.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `arma_tu_fiesta`.`entidad` (
-    `clave_primaria` INT NOT NULL,
-    `atributo` VARCHAR(45) NOT NULL,
-    `clave_foranea` INT NOT NULL,
-    PRIMARY KEY (`id_metodo`),
-    FOREIGN KEY (`clave_foranea`) REFERENCES `arma_tu_fiesta`.`entidad_origen` (`clave_primaria_de_la_entidad`)
-);
