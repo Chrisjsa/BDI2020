755ef72de74e4726d663f2f4d22929852b36bcf0
Chris Suarez <chris.jsa01@gmail.com>
2020-07-19 21:41:23 -0400

    Definiendo categorias

---
+++ queries/insert_categorias.sql
@@ -0,0 +1,23 @@
+INSERT INTO arma_tu_fiesta.categoria (nombre)
+    VALUES
+    ('Floristeria'), -- Aqui esta el producto bouquet
+    ('Mobiliario de Festejo'),
+    ('Mobiliario Longe'),
+    ('Mobiliario de Acrilico y Cristal'),
+    ('Catering'),
+    ('Bebidas'),
+    ('Pasapalos'),
+    ('Minidulces'),
+    ('Utileria'),
+    ('Toldos'),
+    ('Luces'), -- Aqui se incluye el producto velas artesanales
+    ('Tarimas'),
+    ('Personal de Festejo'),
+    ('Transporte'),
+    ('Audivisual'), -- aqui podemos tener servicio fotografia , servicio Dj ardilla
+    ('Decoracion'), -- esta categoria nos sirve para un producto Telas Rojas de o servicio Estilo Barroco
+    ('Centro de Mesa'),
+    ('Diseñador'),
+    ('Costura'),
+    ('Modista')
+;
--- queries/insert_religion.sql
+++ queries/insert_religion.sql
@@ -1,9 +1,3 @@
-CREATE TABLE IF NOT EXISTS `arma_tu_fiesta`.`religion` (
-    `id_religion`           INT NOT NULL AUTO_INCREMENT,
-    `nombre`                VARCHAR(80) NOT NULL,
-    CONSTRAINT `pk_religion` PRIMARY KEY (`id_religion`)
-);
-
 INSERT INTO `arma_tu_fiesta`.`religion` (`nombre`)
     VALUES
     ('Cristianismo'),
