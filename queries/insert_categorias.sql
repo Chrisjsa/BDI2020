9e7f340134135946182d00a9a11972e748d96bef
Tomas <tomasg92@gmail.com>
2020-07-22 13:29:53 -0400

    agregadas categorías: música, fotografía y protocolo

--- queries/insert_categorias.sql
+++ queries/insert_categorias.sql
@@ -1,23 +1,31 @@
-INSERT INTO arma_tu_fiesta.categoria (nombre)
-    VALUES
-    ('Floristeria'), -- Aqui esta el producto bouquet
+INSERT INTO arma_tu_fiesta.categoria
+    (nombre)
+VALUES
+    ('Floristería'),
+    -- Aqui esta el producto bouquet
     ('Mobiliario de Festejo'),
-    ('Mobiliario Longe'),
-    ('Mobiliario de Acrilico y Cristal'),
+    ('Mobiliario Lounge'),
+    ('Mobiliario de Acrílico y Cristal'),
     ('Catering'),
     ('Bebidas'),
     ('Pasapalos'),
     ('Minidulces'),
+    ('Música'),
+    ('Protocolo'),
     ('Utileria'),
     ('Toldos'),
-    ('Luces'), -- Aqui se incluye el producto velas artesanales
+    ('Luces'),
+    -- Aqui se incluye el producto velas artesanales
     ('Tarimas'),
     ('Personal de Festejo'),
     ('Transporte'),
-    ('Audivisual'), -- aqui podemos tener servicio fotografia , servicio Dj ardilla
-    ('Decoracion'), -- esta categoria nos sirve para un producto Telas Rojas de o servicio Estilo Barroco
+    ('Audiovisual'),
+    -- aqui podemos tener servicio fotografia , servicio Dj ardilla
+    ('Decoración'),
+    -- esta categoria nos sirve para un producto Telas Rojas de o servicio Estilo Barroco
     ('Centro de Mesa'),
     ('Diseñador'),
     ('Costura'),
-    ('Modista')
+    ('Modista'),
+    ('Fotografía')
 ;
