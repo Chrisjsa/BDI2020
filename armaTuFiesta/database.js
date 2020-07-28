2ced930b78ea798d8ef5b8eea2c1252ef245f5f5
Tomas <tomasg92@gmail.com>
2020-07-28 03:40:23 -0400

    agregados usuarios dinámicos para la base de datos

--- armaTuFiesta/database.js
+++ armaTuFiesta/database.js
@@ -1,8 +1,28 @@
 const mysql = require("mysql")
 
+const user = process.argv[2]
+let password = undefined
+
+switch (user) {
+  case "tomas":
+    password = "gh1290yu"
+    break
+  case "winkler":
+    password = "4225741"
+    break
+  case "chris":
+    password = "olakase"
+    break
+}
+
+if (!password) {
+  console.log(`\tERR: Usuario "${user}" desconocido`)
+  process.exit(2)
+}
+
 exports.connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   database: "arma_tu_fiesta",
-  password: "4225741", // gh1290yu - olakase - 4225741
+  password,
 })
