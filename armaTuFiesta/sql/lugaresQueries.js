65f7317f258efea54e277e1eef9558e6e75185ee
Tomas <tomasg92@gmail.com>
2020-07-30 04:09:40 -0400

    comentados queries

--- armaTuFiesta/sql/lugaresQueries.js
+++ armaTuFiesta/sql/lugaresQueries.js
@@ -1,5 +1,8 @@
-exports.LEER_LUGARES_ESTADOS = "SELECT id_lugar ID, tipo Tipo, nombre Nombre FROM arma_tu_fiesta.lugar WHERE fk_pertenece = (SELECT fkLugar FROM(SELECT id_lugar as fkLugar FROM arma_tu_fiesta.lugar WHERE tipo = 'Pais' AND nombre = 'Venezuela') as tablaLugar);"
+exports.LEER_LUGARES_ESTADOS =
+  "SELECT id_lugar ID, tipo Tipo, nombre Nombre FROM arma_tu_fiesta.lugar WHERE fk_pertenece = (SELECT fkLugar FROM(SELECT id_lugar as fkLugar FROM arma_tu_fiesta.lugar WHERE tipo = 'Pais' AND nombre = 'Venezuela') as tablaLugar);"
 
-exports.LEER_LUGARES_MUNICIPIOS = "SELECT id_lugar ID, tipo Tipo, nombre Nombre FROM arma_tu_fiesta.lugar WHERE fk_pertenece = 'ID_estado';"
+exports.LEER_LUGARES_MUNICIPIOS =
+  "SELECT id_lugar ID, tipo Tipo, nombre Nombre FROM arma_tu_fiesta.lugar WHERE fk_pertenece = 'ID_estado';"
 
-exports.LEER_LUGARES_PARROQUIAS = "SELECT id_lugar ID, tipo Tipo, nombre Nombre FROM arma_tu_fiesta.lugar WHERE fk_pertenece = 'ID_municipio';"
+exports.LEER_LUGARES_PARROQUIAS =
+  "SELECT id_lugar ID, tipo Tipo, nombre Nombre FROM arma_tu_fiesta.lugar WHERE fk_pertenece = 'ID_municipio';"
