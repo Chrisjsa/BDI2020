0ac702b8d7a0188abe4cc73ce99b966c35ea9371
Tomas <tomasg92@gmail.com>
2020-07-30 01:06:37 -0400

    configuracion del controlador crearCarta

--- armaTuFiesta/controllers/cartas.js
+++ armaTuFiesta/controllers/cartas.js
@@ -23,8 +23,46 @@ exports.crearCarta = (req, res) => {
     dataT2sc,
   } = req.body
 
+  const personas = [
+    dataPpc,
+    dataMpc,
+    dataT1pc,
+    dataT2pc,
+    dataSc,
+    dataPsc,
+    dataMsc,
+    dataT1sc,
+    dataT2sc,
+  ]
+
+  let usuario = undefined
+
+  //BORRAR
+  return res.json("xd")
+
+  // {
+  //   cedula
+  //   p_nombre
+  //   s_nombre
+  //   p_apellido
+  //   s_apellido
+  //   fecha_nacimiento
+  //   sexo
+  //   estado_civil
+  // } solo require id_persona
+
+  // datos del usuario, esto sera un select
+  connection.query("", [], (error, rows) => {
+    if (error) {
+      return res.status(400).send(error.message)
+    }
+    usuario = rows.pop()
+  })
+
+  // todo lo que viene de aqui es un insert
   const success = { msg: "Carta agregada satisfactoriamente" }
 
+  // el primer insert es del usuario
   connection.query("", Object.values(dataPpc), error => {
     if (error) {
       return res.status(400).send(error.message)
@@ -32,11 +70,14 @@ exports.crearCarta = (req, res) => {
     return res.json(success)
   })
 
-  connection.query("", Object.values(dataT2sc), error => {
-    if (error) {
-      return res.status(400).send(error.message)
-    }
-    return res.json(success)
+  // luego es de todos los demas
+  personas.forEach(persona => {
+    connection.query("", Object.values(persona), error => {
+      if (error) {
+        return res.status(400).send(error.message)
+      }
+      return res.json(success)
+    })
   })
 }
 
