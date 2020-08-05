7df0c4bb2307cbda7007632998589658a8c6f97c
John Doe <wjsuarez07@gmail.com>
2020-08-05 13:21:22 -0400

    form de cliente

--- armaTuFiesta/client/src/components/clientes/ClienteForm.js
+++ armaTuFiesta/client/src/components/clientes/ClienteForm.js
@@ -8,6 +8,7 @@ import DatePicker from "react-datepicker"
 
 import "react-datepicker/dist/react-datepicker.css"
 
+import LugarFields from "../lugares/LugarFields"
 // tomas
 // conectar con el estado cliente
 // conectar con acciones
@@ -20,9 +21,32 @@ import "react-datepicker/dist/react-datepicker.css"
 // (presionar enter para enviar valores)
 // agregar el campo de Lugares (<LugarField />)
 
+
+const clearForm = () => {
+  
+}
+
+const handleUpdate = () => {
+  
+}
+
+const handleCreate = () => {
+  
+}
+
+const handleDelete = () => {
+  
+}
+
+const onChange = event => {
+  
+}
+
 let ClienteForm = props => {
   const { handleSubmit } = props
 
+
+  
   const onSubmit = formValues => {
     console.log(formValues) // la consola se abre con CTRL+SHIFT+I
   }
@@ -32,24 +56,66 @@ let ClienteForm = props => {
   // fechaNacimiento y setFechaNacimiento son nombres arbitrarios
   // la primera es el valor, la segunda es la función que la modifica
   // useState() recibe un valor para el estado, en este caso la fecha
+  const [sexo, setSexo] = useState("Masculino")
+  const [estadoCivil, setEstadoCivil] = useState("")
 
   return (
     <Form onSubmit={props.handleSubmit(onSubmit)}>
       <Form className="Row">
+      <LugarFields currentData={false} />
         {/* Cada campo es un Form.Group */}
+        <Form.Row>
         <Form.Group as={Col}>
           <Form.Label>Primer Nombre</Form.Label>
           <Field
             className="form-control"
-            name="p_nombre" // se escribe el atributo con convencio de base de datos (p_nombre, etc...)
+            name="p_nombre" 
             component="input"
-            type="text" // creo que hay tipo number para campos numero
+            type="text" 
           />
         </Form.Group>
 
+        <Form.Group as={Col}>
+          <Form.Label>Segundo Nombre</Form.Label>
+          <Field
+            className="form-control"
+            name="s_nombre" 
+            component="input"
+            type="text" 
+          />
+        </Form.Group>
+        </Form.Row>
+
+        <Form.Row>
+        <Form.Group as={Col}>
+          <Form.Label>Primer Apellido</Form.Label>
+          <Field
+            className="form-control"
+            name="p_apellido" 
+            component="input"
+            type="text" 
+          />
+        </Form.Group>
+
+        <Form.Group as={Col}>
+          <Form.Label>Segundo Apellido</Form.Label>
+          <Field
+            className="form-control"
+            name="s_apellido" 
+            component="input"
+            type="text" 
+          />
+        </Form.Group>
+
+        </Form.Row>
+
+
+
+
+        <Form.Row>
         {/* El date picker siempre es así */}
         <Form.Group as={Col}>
-          <p style={{ marginBottom: "0.5rem" }}>From</p>
+          <p style={{ marginBottom: "0.5rem" }}>Fecha de nacimiento</p>
           <DatePicker
             selected={fechaNacimiento} // asi se llama la variable de estado que declaramos en la linea 27
             onChange={date => setFechaNacimiento(date)} // onChange (en cada seleccion de fecha), la fecha "date" se asigna a fechaDeNacimiento
@@ -60,6 +126,90 @@ let ClienteForm = props => {
             dropdownMode="select"
           />
         </Form.Group>
+
+        <Form.Group as={Col}>
+          <Form.Label>Correo Electronico</Form.Label>
+          <Field
+            className="form-control"
+            name="correo" 
+            component="input"
+            type="text" 
+          />
+        </Form.Group>
+
+        <Form.Group as={Col}>
+          <Form.Label>Telefono</Form.Label>
+          <Field
+            className="form-control"
+            name="numero" 
+            component="input"
+            type="text" 
+          />
+        </Form.Group>
+        </Form.Row>
+        <Form.Row>
+        <fieldset>
+          <Form.Group>
+            <Form.Label className="mr-3">Sex</Form.Label>
+            <Form.Check
+              inline
+              type="radio"
+              label="M"
+              name="sexo"
+              value="Masculino"
+              onChange={e => setSexo(e.target.value)}
+              checked={sexo === "Masculino"}
+            />
+            <Form.Check
+              inline
+              type="radio"
+              label="F"
+              name="sexo"
+              value="Femenino"
+              onChange={e => setSexo(e.target.value)}
+              checked={sexo === "Femenino"}
+            />
+          </Form.Group>
+        </fieldset>
+
+        <Form.Group as={Col}>
+          <Form.Label>Estado civil</Form.Label>
+          <Form.Control
+            as="select"
+            defaultValue="Seleccionar..."
+            name="estadoCivil"
+            value={estadoCivil}
+            onChange={e => setEstadoCivil(e.target.value)}
+          >
+            <option>Seleccionar...</option>
+            <option value="Soltero">Soltero</option>
+            <option value="Casado">Casado</option>
+            <option value="Divorciado">Divorciado</option>
+            <option value="Viudo">Viudo</option>
+          </Form.Control>
+        </Form.Group>
+
+
+
+      </Form.Row>
+
+          {/* <div className="text-center">
+          {currentNotaria ? (
+            <>
+              <Button className="mr-3" onClick={handleUpdate}>
+                Actualizar notaria
+              </Button>
+              <Button onClick={handleDelete} variant="outline-danger">
+                Eliminar notaria
+              </Button>
+            </>
+          ) : (
+            <Button disabled={isNotariaEmpty} onClick={handleCreate}>
+              Agregar notaria
+            </Button>
+          )}
+        </div> */}
+
       </Form>
     </Form>
   )
