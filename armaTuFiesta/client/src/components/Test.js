b4848ef540518e9fc8e8b30d32e6146cd5472b15
Tomas <tomasg92@gmail.com>
2020-08-09 16:45:37 -0400

    reports now working!

--- armaTuFiesta/client/src/components/Test.js
+++ armaTuFiesta/client/src/components/Test.js
@@ -7,17 +7,27 @@ import axios from "axios"
 import { saveAs } from "file-saver"
 
 const Test = () => {
+  const data = {
+    dataPpc: { p_nombre: "Pedro " }, // poner más data si quieres probar
+    dataMpc: { s_nombre: "Sylvia " },
+  }
+
   let feedback = ""
 
   const onClick = async () => {
     try {
-      const res = await axios.post("/api/testing/report", {
-        responseType: "arraybuffer",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/pdf",
-        },
-      })
+      const res = await axios.get(
+        `/api/testing/report?data=${JSON.stringify(data)}`,
+        {
+          responseType: "blob",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/pdf",
+          },
+        }
+      )
+
+      console.log(res)
       // saveAs(new Blob([res.data], { type: "application/pdf" }), "file.pdf")
 
       //   console.log(res.data)
@@ -32,7 +42,10 @@ const Test = () => {
       // link.setAttribute("download", "file.pdf")
       // document.body.appendChild(link)
       // link.click()
-      const url = window.URL.createObjectURL(new Blob([res.data]))
+      const url = window.URL.createObjectURL(
+        new Blob([res.data], { type: "application/pdf" })
+      )
+      window.open(url)
       const link = document.createElement("a")
       link.href = url
       link.setAttribute("download", "file.pdf") //or any other extension
--- armaTuFiesta/routes/testing.js
+++ armaTuFiesta/routes/testing.js
@@ -6,26 +6,20 @@ const { monitor } = require("../middleware/monitor")
 const { auth } = require("../middleware/auth")
 const middleware = [auth, monitor]
 
-var data = {
-  dataPpc: { p_nombre: "John " }, // poner más data si quieres probar
-  dataMpc: { s_nombre: "Helena " },
-}
-
 var options = {
   convertTo: "pdf",
 }
 
 const renderReport = (req, res) => {
+  let data = JSON.parse(req.query.data)
   carbone.render("./reports/simple.txt", data, options, (err, result) => {
     if (err) {
       return console.log(err)
     }
-    fs.writeFileSync("./reports/rendered/result.pdf", result)
-    res.set("Content-Type", "application/pdf")
-    return res.download("./reports/rendered/result.pdf")
+    return res.send(result)
   })
 }
 
-router.post("/report", renderReport)
+router.get("/report", renderReport)
 
 module.exports = router
