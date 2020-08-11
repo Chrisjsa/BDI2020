68b52431e0ea63083b760e98cf561234c3582966
Tomas <tomasg92@gmail.com>
2020-08-11 00:57:39 -0400

    interfaz grafica de reporte lista

--- armaTuFiesta/client/src/components/reportes/ReporteItem.js
+++ armaTuFiesta/client/src/components/reportes/ReporteItem.js
@@ -2,14 +2,13 @@ import React from "react"
 
 import { Card, Button } from "react-bootstrap"
 
-const ReporteItem = ({ title, subtitle, children, handler, fechas }) => {
+const ReporteItem = ({ title, children, handler }) => {
   return (
     <Card className="my-3">
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{subtitle}</Card.Subtitle>
         <Card.Text>{children}</Card.Text>
-        <Button onClick={handler(fechas)}>Descargar</Button>
+        <Button onClick={handler}>Descargar</Button>
       </Card.Body>
     </Card>
   )
--- armaTuFiesta/client/src/components/reportes/Reportes.js
+++ armaTuFiesta/client/src/components/reportes/Reportes.js
@@ -15,49 +15,82 @@ const Reportes = () => {
   const [fechaInicial, setFechaInicial] = useState(new Date())
   const [fechaFinal, setFechaFinal] = useState(new Date())
 
+  // eliminar luego del trabajo de winkler
+
+  const obtenerReporte1 = fechas => {
+    console.log("reporte 1", fechas)
+  }
+  const obtenerReporte2 = fechas => {
+    console.log("reporte 2", fechas)
+  }
+  const obtenerReporte3 = fechas => {
+    console.log("reporte 3", fechas)
+  }
+  const obtenerReporte4 = fechas => {
+    console.log("reporte 4", fechas)
+  }
+
   return (
     <>
       {/* Fechas */}
+      <h2>Elección del período</h2>
       <Form>
-        {/* Fecha inicial */}
-        <Form.Group as={Col}>
-          <p style={{ marginBottom: "0.5rem" }}>Fecha de nacimiento</p>
-          <DatePicker
-            selected={fechaInicial}
-            onChange={date => setFechaInicial(date)}
-            peekNextMonth
-            showMonthDropdown
-            showYearDropdown
-            className="form-control"
-            dropdownMode="select"
-          />
-        </Form.Group>
+        <Form.Row>
+          {/* Fecha inicial */}
+          <Form.Group as={Col}>
+            <p style={{ marginBottom: "0.5rem" }}>Desde</p>
+            <DatePicker
+              selected={fechaInicial}
+              onChange={date => setFechaInicial(date)}
+              peekNextMonth
+              showMonthDropdown
+              showYearDropdown
+              className="form-control"
+              dropdownMode="select"
+            />
+          </Form.Group>
 
-        {/* Fecha final */}
-        <Form.Group as={Col}>
-          <p style={{ marginBottom: "0.5rem" }}>Fecha de nacimiento</p>
-          <DatePicker
-            selected={fechaFinal}
-            onChange={date => setFechaFinal(date)}
-            peekNextMonth
-            showMonthDropdown
-            showYearDropdown
-            className="form-control"
-            dropdownMode="select"
-          />
-        </Form.Group>
+          {/* Fecha final */}
+          <Form.Group as={Col}>
+            <p style={{ marginBottom: "0.5rem" }}>Hasta</p>
+            <DatePicker
+              selected={fechaFinal}
+              onChange={date => setFechaFinal(date)}
+              peekNextMonth
+              showMonthDropdown
+              showYearDropdown
+              className="form-control"
+              dropdownMode="select"
+            />
+          </Form.Group>
+        </Form.Row>
       </Form>
 
       {/* Reportes */}
       <ReporteItem
         title="Reporte 1"
-        subtitle="Reporte 1"
-        handler={() => alert(`hello reporte`)}
-        fechas={{ fechaInicial, fechaFinal }}
+        handler={e => obtenerReporte1({ fechaInicial, fechaFinal })}
       >
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
-        quisquam cum alias sunt ipsam velit suscipit nobis doloremque nulla est
-        non esse enim, accusamus, eos harum id! Vitae, assumenda nulla.
+        Total de fiestas por mes
+      </ReporteItem>
+      <ReporteItem
+        title="Reporte 2"
+        handler={e => obtenerReporte2({ fechaInicial, fechaFinal })}
+      >
+        Detalle de todas las contrataciones a terceros por período de tiempo,
+        indicando fecha y costo
+      </ReporteItem>
+      <ReporteItem
+        title="Reporte 3"
+        handler={e => obtenerReporte3({ fechaInicial, fechaFinal })}
+      >
+        Total de ingresos y egresos por periodo de tiempo
+      </ReporteItem>
+      <ReporteItem
+        title="Reporte 4"
+        handler={e => obtenerReporte4({ fechaInicial, fechaFinal })}
+      >
+        Top 10 de servicios tercerizados contratados por periodo de tiempo.
       </ReporteItem>
     </>
   )
