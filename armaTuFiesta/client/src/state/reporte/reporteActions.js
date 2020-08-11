e5bb9cb0406edad9a87e711ee81cc515776a48ad
John Doe <wjsuarez07@gmail.com>
2020-08-11 01:00:34 -0400

    state de reportes listos

--- armaTuFiesta/client/src/state/reporte/reporteActions.js
+++ armaTuFiesta/client/src/state/reporte/reporteActions.js
@@ -2,29 +2,74 @@ import {
   ERROR_REPORTE,
   LOADING_REPORTE,
   OBTENER_REPORTE_1,
-} from "./reporteTypes"
-import axios from "axios"
-import setAuthToken from "../../utils/setAuthToken"
+  OBTENER_REPORTE_2,
+  OBTENER_REPORTE_3,
+  OBTENER_REPORTE_4,
+} from "./reporteTypes";
+import axios from "axios";
+import setAuthToken from "../../utils/setAuthToken";
 
-import moment from "moment"
+import moment from "moment";
 
-const config = { headers: { "Content-Type": "application/json" } }
+const config = { headers: { "Content-Type": "application/json" } };
 
-export const setLoading = () => dispatch => {
-  return dispatch({ type: LOADING_REPORTE })
-}
+export const setLoading = () => (dispatch) => {
+  return dispatch({ type: LOADING_REPORTE });
+};
 
-export const obtenerReporte1 = fechas => async dispatch => {
-  const fechaInicial = moment(fechas.fechaInicial).format("MM-DD-YYYY")
-  const fechaFinal = moment(fechas.fechaFinal).format("MM-DD-YYYY")
+export const obtenerReporte1 = (fechas) => async (dispatch) => {
+  const fechaInicial = moment(fechas.fechaInicial).format("MM-DD-YYYY");
+  const fechaFinal = moment(fechas.fechaFinal).format("MM-DD-YYYY");
 
-  setLoading()(dispatch)
+  setLoading()(dispatch);
   try {
     const res = await axios.get(
       `api/reportes/1?fechaInicial=${fechaInicial}&fechaFinal=${fechaFinal}`
-    )
-    dispatch({ type: OBTENER_REPORTE_1, payload: res.data })
+    );
+    dispatch({ type: OBTENER_REPORTE_1, payload: res.data });
   } catch (error) {
-    dispatch({ type: ERROR_REPORTE, payload: error.response.msg })
+    dispatch({ type: ERROR_REPORTE, payload: error.response.msg });
   }
-}
+};
+
+export const obtenerReporte2 = (fechas) => async (dispatch) => {
+  const fechaInicial = moment(fechas.fechaInicial).format("MM-DD-YYYY");
+  const fechaFinal = moment(fechas.fechaFinal).format("MM-DD-YYYY");
+  setLoading()(dispatch);
+  try {
+    const res = await axios.get(
+      `api/reportes/1?fechaInicial=${fechaInicial}&fechaFinal=${fechaFinal}`
+    );
+    dispatch({ type: OBTENER_REPORTE_2, payload: res.data });
+  } catch (error) {
+    dispatch({ type: ERROR_REPORTE, payload: error.response.msg });
+  }
+};
+
+export const obtenerReporte3 = (fechas) => async (dispatch) => {
+  const fechaInicial = moment(fechas.fechaInicial).format("MM-DD-YYYY");
+  const fechaFinal = moment(fechas.fechaFinal).format("MM-DD-YYYY");
+  setLoading()(dispatch);
+  try {
+    const res = await axios.get(
+      `api/reportes/1?fechaInicial=${fechaInicial}&fechaFinal=${fechaFinal}`
+    );
+    dispatch({ type: OBTENER_REPORTE_3, payload: res.data });
+  } catch (error) {
+    dispatch({ type: ERROR_REPORTE, payload: error.response.msg });
+  }
+};
+
+export const obtenerReporte4 = (fechas) => async (dispatch) => {
+  const fechaInicial = moment(fechas.fechaInicial).format("MM-DD-YYYY");
+  const fechaFinal = moment(fechas.fechaFinal).format("MM-DD-YYYY");
+  setLoading()(dispatch);
+  try {
+    const res = await axios.get(
+      `api/reportes/1?fechaInicial=${fechaInicial}&fechaFinal=${fechaFinal}`
+    );
+    dispatch({ type: OBTENER_REPORTE_4, payload: res.data });
+  } catch (error) {
+    dispatch({ type: ERROR_REPORTE, payload: error.response.msg });
+  }
+};
