8bbd0ad67799ea4d2d1882dd8c3d7e0a1bd159b2
Tomas <tomasg92@gmail.com>
2020-08-11 22:05:27 -0400

    mejorada interfaz de metodo de pago

--- armaTuFiesta/client/src/components/metodos/Metodos.js
+++ armaTuFiesta/client/src/components/metodos/Metodos.js
@@ -1,96 +1,30 @@
 import React from "react"
 import Cards from "react-credit-cards"
 import "react-credit-cards/es/styles-compiled.css"
+
+import { Card } from "react-bootstrap"
+
 const Metodos = () => {
-  const [state, setState] = React.useState({
-    number: "",
-    name: "",
-    expiry: "",
-    cvc: "",
-    focus: "",
-  })
-
-  const handleInputChange = (e) => {
-    setState({
-      ...state,
-      [e.target.name]: e.target.value,
-    })
-  }
-
-  const handleFocusChange = (e) => {
-    setState({
-      ...state,
-      focus: e.target.name,
-    })
-  }
-
   return (
-    <div className="card">
-      <div className="card-body">
-        <Cards
-          number={state.number}
-          name={state.name}
-          expiry={state.expiry}
-          cvc={state.cvc}
-          focused={state.focus}
-        />
-        <form>
-          <div className="form-group">
-            <label htmlFor="number">Número de la tarjeta</label>
-            <input
-              type="text"
-              name="number"
-              id="number"
-              maxLength="16"
-              className="form-control"
-              onChange={handleInputChange}
-              onFocus={handleFocusChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="name">Nombre</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              maxLength="30"
-              className="form-control"
-              onChange={handleInputChange}
-              onFocus={handleFocusChange}
-            />
-          </div>
-          <div className="form-row">
-            <div className="form-group col-md-6">
-              <label htmlFor="expiry">Fecha de expiración</label>
-              <input
-                type="text"
-                name="expiry"
-                id="expiry"
-                maxLength="4"
-                className="form-control"
-                onChange={handleInputChange}
-                onFocus={handleFocusChange}
-              />
-            </div>
-            <div className="form-group col-md-6">
-              <label htmlFor="cvc">CVC</label>
-              <input
-                type="text"
-                name="cvc"
-                id="cvc"
-                maxLength="4"
-                className="form-control"
-                onChange={handleInputChange}
-                onFocus={handleFocusChange}
-              />
-            </div>
-          </div>
-
-          <button type="button" className="btn btn-success btn-block btn-lg">
-            Agregar
-          </button>
-        </form>
-      </div>
+    <div>
+      <Cards
+        number={"11112222333344445555"}
+        name={"hola1"}
+        expiry={"10/10"}
+        cvc={123}
+      />
+      <Cards
+        number={"11112222333344445555"}
+        name={"hola2"}
+        expiry={"10/10"}
+        cvc={123}
+      />
+      <Cards
+        number={"11112222333344445555"}
+        name={"hola13"}
+        expiry={"10/10"}
+        cvc={432}
+      />
     </div>
   )
 }
