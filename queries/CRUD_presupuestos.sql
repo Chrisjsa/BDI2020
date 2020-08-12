eff76ced5fa769bd4cc97bf3f0b70ec7db19f67e
Chris Suarez <chris.jsa01@gmail.com>
2020-08-12 18:57:40 -0400

    actualizacion listar detalles

--- queries/CRUD_presupuestos.sql
+++ queries/CRUD_presupuestos.sql
@@ -6,7 +6,7 @@ WHERE eve.fk_tema = tem.id_tema AND ordeve.fk_evento = eve.id_evento AND ordeve.
 
 -- SELECCIONAR DATOS RELACIONADOS A UN PRESUPUESTO
 
-SET @presupuesto = 1;
+SET @presupuesto = 25;
 
 SELECT usu.username as Usuario, eve.nombre as Evento, tem.nombre as Tema, ordeve.cantidad_invitados as Num_Invitados, ordeve.fecha_realizacion as Fecha_Realizacion, pre.id_presupuesto as Num_Presupuesto, pre.total as Costo_Presupuesto, presta.fecha as Fecha_Presupuesto, est.nombre as Presupuesto_Estatus
 FROM arma_tu_fiesta.tema as tem, arma_tu_fiesta.evento as eve, arma_tu_fiesta.orden_evento as ordeve, arma_tu_fiesta.presupuesto as pre, arma_tu_fiesta.usuario as usu, arma_tu_fiesta.presupuesto_estatus as presta, arma_tu_fiesta.estatus as est
@@ -22,13 +22,13 @@ WHERE eve.fk_tema = tem.id_tema AND ordeve.fk_evento = eve.id_evento AND ordeve.
 
 -- OBTENER DETALLES DE UN PRESUPUESTO (Productos o Servicios)
 
-SET @presupuesto = 1;
+SET @orden_evento = 25;
 
-SELECT det.fk_presupuesto as Num_Presupuesto, ser.nombre as 'Servicio/Producto', det.precio_unitario, det.cantidad, det.precio
-FROM arma_tu_fiesta.detalle as det, arma_tu_fiesta.servicio_tercerizado as ser
-WHERE det.fk_presupuesto = @presupuesto and det.fk_servicio = ser.id_servicio
+SELECT pre.fk_orden_evento as id_orden_evento, det.fk_presupuesto as Num_Presupuesto, ser.nombre as 'Servicio/Producto', det.costo_unitario, det.cantidad, det.costo, det.precio as a_cobrar
+FROM arma_tu_fiesta.presupuesto as pre, arma_tu_fiesta.detalle as det, arma_tu_fiesta.servicio_tercerizado as ser
+WHERE pre.fk_orden_evento = @orden_evento AND det.fk_presupuesto = @presupuesto and det.fk_servicio = ser.id_servicio
 UNION
-SELECT det.fk_presupuesto as Num_Presupuesto, pro.nombre as 'Servicio/Producto', det.precio_unitario, det.cantidad, det.precio
-FROM arma_tu_fiesta.detalle as det, arma_tu_fiesta.producto as pro
-WHERE det.fk_presupuesto = @presupuesto and det.fk_producto = pro.id_producto;
+SELECT pre.fk_orden_evento as id_orden_evento, det.fk_presupuesto as Num_Presupuesto, pro.nombre as 'Servicio/Producto', det.costo_unitario, det.cantidad, det.costo, det.precio as a_cobrar
+FROM arma_tu_fiesta.presupuesto as pre, arma_tu_fiesta.detalle as det, arma_tu_fiesta.producto as pro
+WHERE pre.fk_orden_evento = @orden_evento AND det.fk_presupuesto = @presupuesto and det.fk_producto = pro.id_producto;
 
