4cf35e49a453ca923c7130815f123cd1ac076fdf
Chris Suarez <chris.jsa01@gmail.com>
2020-08-12 23:01:27 -0400

    manejo boda

--- queries/manejo_boda.sql
+++ queries/manejo_boda.sql
@@ -1,12 +1,12 @@
 -- Listar todos los datos de los templos
 
 SELECT *
-FROM arma_tu_fiesta.locacion as loc, arma_tu_fiesta.templo as tem
-WHERE loc.tipo = 'Templo' AND tem.fk_templo = loc.id_locacion;
+FROM arma_tu_fiesta.locacion as loc, arma_tu_fiesta.templo as tem, arma_tu_fiesta.coordenada as coor
+WHERE loc.tipo = 'Templo' AND tem.fk_templo = loc.id_locacion AND coor.fk_locacion = loc.id_locacion;
 
 -- Lista los cursos disponibles con el id del templo catolico escogido
 
-SET @templo = 30;
+SET @templo = ID_TEMPLO;
 
 SELECT temp.id_locacion as id_templo, cur.id_curso, temp.nombre as nombre_templo, cur.nombre
 FROM arma_tu_fiesta.locacion as temp, arma_tu_fiesta.curso as cur
