219c7a58487a4af17c9b8de5a9d7be43769405d9
John Doe <wjsuarez07@gmail.com>
2020-08-14 17:58:59 -0400

    form nuevo evento

--- armaTuFiesta/client/src/components/eventos/NuevoEventoModal.js
+++ armaTuFiesta/client/src/components/eventos/NuevoEventoModal.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react"
 
-import { Button, Modal } from "react-bootstrap"
+import { Button, Modal, Form, Col } from "react-bootstrap"
 
 const NuevoEventoModal = () => {
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
+  const [tipoFiesta, setTipoFiesta] = useState("")
 
+  const packData = () => ({
+    tipoFiesta,
+  })
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
@@ -18,6 +22,22 @@ const NuevoEventoModal = () => {
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Form.Group as={Col}>
+          <Form.Label>Tipo de Fiesta</Form.Label>
+          <Form.Control
+            as="select"
+            defaultValue="Seleccionar..."
+            name="tipoFiesta"
+            value={tipoFiesta}
+            onChange={(e) => setTipoFiesta(e.target.value)}
+          >
+            <option>Seleccionar...</option>
+            <option value="Soltero">Soltero</option>
+            <option value="Casado">Casado</option>
+            <option value="Divorciado">Divorciado</option>
+            <option value="Viudo">Viudo</option>
+          </Form.Control>
+        </Form.Group>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
