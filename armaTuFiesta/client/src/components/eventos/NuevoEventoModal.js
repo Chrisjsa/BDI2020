0f622629492472c9be1921114041ea90049a5d03
John Doe <wjsuarez07@gmail.com>
2020-08-14 18:30:06 -0400

    form nuevo eventos

--- armaTuFiesta/client/src/components/eventos/NuevoEventoModal.js
+++ armaTuFiesta/client/src/components/eventos/NuevoEventoModal.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react"
 
 import { Button, Modal, Form, Col } from "react-bootstrap"
+import LugarFields from "../lugares/LugarFields"
+
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
 
 const NuevoEventoModal = () => {
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
   const [tipoFiesta, setTipoFiesta] = useState("")
+  const [fecha, setFecha] = useState(new Date())
 
-  const packData = () => ({
-    tipoFiesta,
-  })
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
@@ -32,12 +34,53 @@ const NuevoEventoModal = () => {
             onChange={(e) => setTipoFiesta(e.target.value)}
           >
             <option>Seleccionar...</option>
-            <option value="Soltero">Soltero</option>
-            <option value="Casado">Casado</option>
-            <option value="Divorciado">Divorciado</option>
-            <option value="Viudo">Viudo</option>
+            <option value="Jefatura">Jefatura</option>
+            <option value="Templo">Templo</option>
+            <option value="Notaria">Notaria</option>
+            <option value="Restaurante">Restaurante</option>
           </Form.Control>
         </Form.Group>
+        <LugarFields />
+        <Form.Group as={Col}>
+          <Form.Label>Restaurante</Form.Label>
+          <Form.Control
+            as="select"
+            defaultValue="Seleccionar..."
+            name="tipoFiesta"
+            value={tipoFiesta}
+            onChange={(e) => setTipoFiesta(e.target.value)}
+          >
+            <option>Seleccionar...</option>
+            <option value="Jefatura">Jefatura</option>
+            <option value="Templo">Templo</option>
+            <option value="Notaria">Notaria</option>
+            <option value="Restaurante">Restaurante</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Row>
+          <div className="form-group">
+            <label htmlFor="number">Número de Invitados</label>
+            <input
+              type="text"
+              name="number"
+              id="number"
+              maxLength="4"
+              className="form-control"
+            />
+          </div>
+          <Form.Group as={Col}>
+            <p style={{ marginBottom: "0.5rem" }}>Fecha de realizacion</p>
+            <DatePicker
+              selected={fecha}
+              onChange={(date) => setFecha(date)}
+              peekNextMonth
+              showMonthDropdown
+              showYearDropdown
+              className="form-control"
+              dropdownMode="select"
+            />
+          </Form.Group>
+        </Form.Row>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
