2e5bbce481e170fdf5670f8d4b867f875857717d
John Doe <wjsuarez07@gmail.com>
2020-08-15 12:26:30 -0400

    leer e insertar citas

--- armaTuFiesta/client/src/state/cita/citaActions.js
+++ armaTuFiesta/client/src/state/cita/citaActions.js
@@ -1,4 +1,4 @@
-import { LEER_SERVICIOS } from "./citaTypes"
+import { LEER_SERVICIOS, INSERTAR_CITA, LEER_CITAS_USUARIO } from "./citaTypes"
 
 import axios from "axios"
 import { errorTimeOut } from "../../utils/"
@@ -9,6 +9,26 @@ export const leerServicios = () => async (dispatch) => {
   try {
     const res = await axios.get("api/citas/leerServicios")
     dispatch({ type: LEER_SERVICIOS, payload: res.data })
+  } catch (error) {
+    dispatch({ type: ERROR_CITA, payload: error.response.msg })
+  }
+}
+
+export const insertarCita = (cita) => async (dispatch) => {
+  setLoading()(dispatch)
+  try {
+    const res = await axios.post("api/citas/insertarCita", cita, config)
+    dispatch({ type: INSERTAR_CITA, payload: res.data })
+  } catch (error) {
+    dispatch({ type: ERROR_CITA, payload: error.response.msg })
+  }
+}
+
+export const leerCitasUsuario = () => async (dispatch) => {
+  setLoading()(dispatch)
+  try {
+    const res = await axios.get("api/citas/leerCitasUsuario")
+    dispatch({ type: LEER_CITAS_USUARIO, payload: res.data })
   } catch (error) {
     dispatch({ type: ERROR_CITA, payload: error.response.msg })
   }
