99518186a85ebe940fa40482ed047d7b60971d54
Chris Suarez <chris.jsa01@gmail.com>
2020-08-14 20:44:01 -0400

    agregar permisos a roles

--- queries/insert_summary.sql
+++ queries/insert_summary.sql
@@ -29107,8 +29107,6 @@ INSERT INTO arma_tu_fiesta.servicio_tercerizado (nombre, descripcion, precio, fk
 
 -- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-
-
 INSERT INTO arma_tu_fiesta.permiso
     (nombre)
 VALUES
@@ -29128,37 +29126,166 @@ VALUES
     ("eliminarServicioTercerizado"),
     ("actualizarServicioTercerizado"),
     ("leerServicioTercerizado"),
-    ("insertarProveedor"),
-    ("eliminarProveedor"),
-    ("actualizarProveedor"),
-    ("leerProveedor")
-;
+    ("obtenerReporte"),
+    ("asignarRolPermisos"),
+    ("eliminarRol"),
+    ("actualizarRol"),
+    ("leerRoles"),
+    ("insertarTip"),
+    ("eliminarTip"),
+    ("actualizarTip"),
+    ("leerTip");
 
-SET @superuser_id = (SELECT id_rol
+-- (s.permisos) ids de permisos
+SET @insertarNotaria = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "insertarNotaria");
+
+SET @leerNotaria = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "leerNotaria");
+
+SET @actualizarNotaria = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "actualizarNotaria");
+
+SET @eliminarNotaria = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "eliminarNotaria");
+
+SET @insertarCliente = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "insertarCliente");
+
+SET @leerCliente = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "leerCliente");
+
+SET @actualizarCliente = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "actualizarCliente");
+
+SET @eliminarCliente = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "eliminarCliente");
+
+SET @insertarEvento = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "insertarEvento");
+
+SET @leerEvento = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "leerEvento");
+
+SET @actualizarEvento = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "actualizarEvento");
+
+SET @eliminarEvento = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "eliminarEvento");
+
+SET @insertarTip = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "insertarTip");
+
+SET @leerTip = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "leerTip");
+
+SET @actualizarTip = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "actualizarTip");
+
+SET @eliminarTip = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "eliminarTip");
+
+SET @asignarRolPermisos = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "asignarRolPermisos");
+
+SET @leerRoles = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "leerRoles");
+
+SET @actualizarRol = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "actualizarRol");
+
+SET @eliminarRol = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "eliminarRol");
+
+SET @leerReporte = (SELECT id_permiso
+FROM permiso
+WHERE nombre = "leerReporte");
+
+-- (s.roles) ids de roles
+SET @rol_superuser_id = (SELECT id_rol
 FROM rol
 WHERE nombre = "Superuser");
 
+
+SET @rol_cliente_id = (SELECT id_rol
+FROM rol
+WHERE nombre = "Cliente");
+
+SET @rol_empleado_id = (SELECT id_rol
+FROM rol
+WHERE nombre = "Empleado");
+
+-- empleado
+
+-- (s.insert) inicio del insert
 INSERT INTO rol_permiso
     (fk_rol, fk_permiso)
 values
-    (@superuser_id, 1),
-    (@superuser_id, 2),
-    (@superuser_id, 3),
-    (@superuser_id, 4),
-    (@superuser_id, 5),
-    (@superuser_id, 6),
-    (@superuser_id, 7),
-    (@superuser_id, 8),
-    (@superuser_id, 9),
-    (@superuser_id, 10),
-    (@superuser_id, 11),
-    (@superuser_id, 12),
-    (@superuser_id, 13),
-    (@superuser_id, 14),
-    (@superuser_id, 15),
-    (@superuser_id, 16),
-    (@superuser_id, 17),
-    (@superuser_id, 18),
-    (@superuser_id, 19),
-    (@superuser_id, 20)
+    (@superuser_id, @insertarCliente),
+    (@superuser_id, @eliminarCliente),
+    (@superuser_id, @actualizarCliente),
+    (@superuser_id, @leerCliente),
+    (@superuser_id, @insertarNotaria),
+    (@superuser_id, @eliminarNotaria),
+    (@superuser_id, @actualizarNotaria),
+    (@superuser_id, @leerNotaria),
+    (@superuser_id, @insertarEvento),
+    (@superuser_id, @eliminarEvento),
+    (@superuser_id, @actualizarEvento),
+    (@superuser_id, @leerEvento),
+    (@superuser_id, @insertarTip),
+    (@superuser_id, @eliminarTip),
+    (@superuser_id, @actualizarTip),
+    (@superuser_id, @leerTip),
+    (@superuser_id, @asignarRolPermisos),
+    (@superuser_id, @eliminarRol),
+    (@superuser_id, @actualizarRol),
+    (@superuser_id, @leerRoles),
+    (@superuser_id, @obtenerReporte),
+
+    -- cliente
+    (@cliente_id, @actualizarCliente),
+    (@cliente_id, @leerCliente),
+    (@cliente_id, @leerNotaria),
+    (@cliente_id, @insertarEvento),
+    (@cliente_id, @actualizarEvento),
+    (@cliente_id, @leerEvento),
+    (@cliente_id, @leerTip),
+
+    -- empleado
+    (@empleado_id, @insertarCliente),
+    (@empleado_id, @eliminarCliente),
+    (@empleado_id, @actualizarCliente),
+    (@empleado_id, @leerCliente),
+    (@empleado_id, @insertarNotaria),
+    (@empleado_id, @eliminarNotaria),
+    (@empleado_id, @actualizarNotaria),
+    (@empleado_id, @leerNotaria),
+    (@empleado_id, @insertarTip),
+    (@empleado_id, @eliminarTip),
+    (@empleado_id, @actualizarTip),
+    (@empleado_id, @leerTip),
+    (@empleado_id, @obtenerReporte)
+
+;
 
